7a0aec99b350cde02fc2d75f2b63c9435a91878e
Jemila <jemila.abulhawa@gmail.com>
2017-10-22 19:20:37 +0100

    update repo

--- script.js
+++ script.js
@@ -1,72 +1,91 @@
+// ------ GLOBAL VARIABLES ------ //
+
 let output = "";
 let cacheNum = "";
 let workingEquation = "";
+const operators = ['/', '*', '-', '+'];
+
+// ------ PRIMARY FUNCTIONS ------ //
 
 const calc = button => {
-  console.log(button);
-  output += button.toString();
-  updateHTML();
+  if (output.length <= 13) {
+    output += button.toString();
+    workingEquation = cacheNum + output;
+    updateHTML(output);
+  } else {
+    console.log("character limit reached")
+  }
+}
+
+const otherFunc = operator => {
+  const val = validation(cacheNum.charAt(cacheNum.length - 1));
+  console.log(val);
+  if (!val) {
+    cacheNum += output + " " + operator + " ";
+    workingEquation = cacheNum;
+    updateHTML(output);
+    output = "";
+  } else {
+    console.log("two operators")
+  }
 }
 
 const reset = () => {
-  output = "";
-  updateHTML();
-  cacheNum = "";
-  document.getElementById("calculation").innerHTML = 0;
-  document.getElementById("working-equation").innerHTML = 0;
-}
-
-const add = () => {
-  cacheNum += output + "+";
-  output = "";
-}
-
-const minus = () => {
-  cacheNum += output + "-";
-  output = "";
-}
-
-const times = () => {
-  cacheNum += output + "*";
-  output = "";
-}
-
-const divide = () => {
-  cacheNum += output + "/";
-  output = "";
-}
-
-const decimal = () => {
-  output += ".";
-  document.getElementById("calculation").value = output;
+    output = "";
+    updateHTML(output);
+    cacheNum = "";
+    document.getElementById("calculation").innerHTML = 0;
+    document.getElementById("working-equation").innerHTML = 0;
 }
 
 const equals = () => {
-  cacheNum += output;
-  console.log(cacheNum);
-  document.getElementById("calculation").innerHTML = eval(cacheNum);
-  cacheNum = eval(cacheNum);
-  output = "";
-}
-
-const signtoggle = () => {
-  output = document.getElementById("calculation").innerHTML *= -1;
-  updateHTML();
-  cacheNum = output;
-  output = "";
+    cacheNum += output;
+    cacheNum = eval(cacheNum).toString();
+    workingEquation = cacheNum;
+    updateHTML(cacheNum);
+    output = "";
 }
 
 const backspace = () => {
-  output = output.substring(0, output.length - 1);
-  updateHTML();
+    output = output.substring(0, output.length - 1);
+    workingEquation = workingEquation.substring(0, workingEquation.length - 1);
+    updateHTML(output);
+    updateHTML(workingEquation);
+}
+
+// ------ OPERATOR FUNCTIONS ------ //
+
+const add = () => otherFunc("+");
+const minus = () => otherFunc("-");
+const times = () => otherFunc("*");
+const divide = () => otherFunc("/");
+
+// ------ UPDATE NUMBER FUNCTIONS ------ //
+
+const signtoggle = () => {
+    output = document.getElementById("calculation").innerHTML *= -1;
+    updateHTML();
+    cacheNum = output;
+    output = "";
+  }
+
+const decimal = () => {
+    output += ".";
+    document.getElementById("calculation").value = output;
 }
 
 const percentage = () => {
-  output /= 100;
-  updateHTML();
+    output /= 100;
+    updateHTML(output);
 }
 
-const updateHTML = () => {
-  document.getElementById("calculation").innerHTML = output;
+// ------ UPDATE UI ------ //
+
+const updateHTML = value => {
+  document.getElementById("calculation").innerHTML = value;
   document.getElementById("working-equation").innerHTML = workingEquation;
 }
+
+// ------ VALIDATION ------ //
+
+const validation = lastElement => operators.includes(lastElement);
